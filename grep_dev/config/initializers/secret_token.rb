e3a2e34ea1f6458461b2c17d50fd3e3b9abe91c4
Praveena G <praveena.gunasekhar@gmail.com>
2013-12-06 00:55:50 +0530

    Praveena | #1 | Finished setup app. yay !!

---
+++ grep_dev/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrepDev::Application.config.secret_key_base = '702ec75b7ec80ecd659e447ca7e78960f4e7e50d3d7bf250313980248ae28f51b83f5abcdb5505f2d73e0a98798de1658513790daba843b204a3af74d206d6d5'
---
+++ grep_dev/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrepDev::Application.config.session_store :cookie_store, key: '_grep_dev_session'
