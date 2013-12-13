550e6aa876123212d70a4fc5097a2a8f1acb9ede
Praveena G <praveena.gunasekhar@gmail.com>
2013-12-13 19:02:21 +0530

    Integrated mongo

--- Gemfile
+++ Gemfile
@@ -1,10 +1,9 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
-gem 'mongo'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'mongoid', '~> 4.0.0.alpha1', github: 'mongoid/mongoid'
+gem 'bson', '~> 2.0.0.rc2'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
@@ -27,9 +26,8 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
-group :doc do
-  # bundle exec rake doc:rails generates the API under doc/api.
-  gem 'sdoc', require: false
+group :test do
+  gem "rspec-rails"
 end
 
 # Use ActiveModel has_secure_password
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'rails/all'
+
+
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
+require "mongoid"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,6 +14,7 @@ Bundler.require(:default, Rails.env)
 
 module GrepDev
   class Application < Rails::Application
+Mongoid.load!(Rails.root.to_s+"/config/mongoid.yml")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
